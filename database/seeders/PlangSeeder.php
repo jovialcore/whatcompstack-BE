6c7dc951051f6928018903096f66e04a54ec3210
jovialcore <jollydosky@gmail.com>
2023-06-29 13:16:52 +0100

    added programming language lists

---
+++ database/seeders/PlangSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plang;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+        $programmingLanguages = [
+            'PHP',
+            'Python',
+            'Ruby',
+            'Java',
+            'C#',
+            'Go',
+            'Rust',
+            'Node.js',
+            'JavaScript',
+            'Perl',
+            'C++',
+            'C',
+            'Swift',
+            'Kotlin',
+            'Scala',
+            'Haskell',
+            'Erlang',
+            'Groovy',
+            'Objective-C',
+            'Lua',
+            'R',
+            'Julia',
+        ];
+
+        foreach ($programmingLanguages as $language) {
+
+            Plang::create(['name' => $language]);
+        }
+    }
+}
