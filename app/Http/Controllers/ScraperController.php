f95733172af63c18fc0b41d670ac5439f8db9fbf
jovialcore <jollydosky@gmail.com>
2023-07-08 22:21:50 +0100

    i tried

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -107,44 +107,68 @@ class ScraperController extends Controller
             // push (merge) the matching item to the result  array
             $result = array_merge($result, $matchedKeys);
         }
-        array_splice($result, 0);
-        // lets get frameworks 
+       // array_splice($result, 0); // assuming there is no programming language 
+        //lets get frameworks 
+
         $result[] = 'Spring Boot';
         $result[] = 'Laravel';
+
+        $result[] = 'Ruby on Rails';
+        $result[] = 'Django';
+        $result[] = 'Flask';
+
         $result[] = 'Symfony';
         $result[] = 'Grails';
         $result[] = 'Swift';
         $result[] = 'Lua';
+
         $result[] = 'CakePHP';
+        $result[] = 'Express.js';
 
 
-
+        //  dd($result);
         /** ######    Group frameworks into their respective programming languages (as Assoc Array) #######  */
         $k = [];
 
+
+        // $arr = array_reduce($result, function ($accum, $item) use ($be_format_for_db, $result) {
+        //     if (array_key_exists($item, $be_format_for_db)) {
+        //         if (!isset($accum[$item])) {
+        //             $accum[$item] = array_intersect($be_format_for_db[$item], $result);
+        //         }
+        //     } else {
+        //         $va = $item;
+        //         dd($item);
+        //         $item = array_search($item, $be_format_for_db);
+               
+        //         $accum[$item][] = $va;
+        //     }
+
+
+
+        //     return $accum;
+        // }, []);
+
+      
         foreach ($result as $key => $value) {
 
             // check if p_lang from scraped results match that of keys in the speculative db format arrangment
-            if (array_key_exists($value, $be_format_for_db)) {
-                // the framework
-                $framework = array_intersect($be_format_for_db[$value], $result);
-                // then use the $value i.e programming langauge and assign the returned framework inside the new collector: $k array
-                $k[$value] = $framework;
-                // if no programming language are found, that means we have only frameworks, then assign the scrapped framework results to their respective programming languages  
-            } else {
-                foreach ($be_format_for_db as $key => $framework) {
+        
+                foreach ($be_format_for_db as $keyy => $framework) {
 
-                    
-                    if (in_array($value, $be_format_for_db[$key])) {
-                        $k[$key][] = $value;
+                    // cater for if we have progrmamming language in the array before so we avoid duplicates of values i.e frameworks e.g ( [PHP => 'laravel', 'cakephp', 'laravel])
+                    if (in_array($value, $be_format_for_db[$keyy])) {
+                        $k[$keyy][] = $value;
+                       
+                    } else{
+                        $k[] = $value;
+                        break;
                     }
                 }
-            }
+            
         }
-        // if no programming language are found, it is here that I need to cook ! 
+dd($k);
 
-
-        dd($k);
         // lets assume we have the Id of the company we want to save
         $company = $company->with('plangs.frameworks.companies', 'frameworks')->find(2);
         $allPlangs = Plang::with('frameworks')->get();
@@ -152,74 +176,63 @@ class ScraperController extends Controller
 
         // if the language is already in the database
 
-        // if ($company->plangs->count() < 0) {
-        //     foreach ($company->plangs as $progrLang) {
+        if ($company->plangs->count() < 0) {
+            foreach ($company->plangs as $progrLang) {
 
-        //         if (array_key_exists($progrLang->name, $k)) {
-
-
-        //             // just update the rating coulmn on pivot table                       // add plus one to the rating  column
-
-        //             $company->plangs()->updateExistingPivot($progrLang->id, ['rating' => $progrLang->pivot->rating + 1]);
-
-        //             // if there are any frameworks and they are what we had before ? do the following
-        //             if (!empty($k[$progrLang->name])) {
-
-        //                 // loop through the ssupposedly frameworks that we have
-        //                 foreach ($k[$progrLang->name] as $frameworkName) {
-        //                     // get the related framework id
-        //                     $frameworkId = $progrLang->frameworks->where('name', $frameworkName)->first()->id;
-        //                     // loop through company frameworks and update the pivot table of ids that match 
-        //                     foreach ($company->frameworks as $fw) {
-        //                         $company->frameworks()->updateExistingPivot($frameworkId, ['rating' => $fw->pivot->rating + 1]);
-        //                     }
-        //                 }
-        //             }
-        //         } else {
-
-        //             $company->plangs()->attach($progrLang->id, ['rating' => 0]);
-        //         }
+                if (array_key_exists($progrLang->name, $k)) {
 
 
+                    // just update the rating coulmn on pivot table                       // add plus one to the rating  column
 
+                    $company->plangs()->updateExistingPivot($progrLang->id, ['rating' => $progrLang->pivot->rating + 1]);
 
-        //     }
-        // } else {
-        //     foreach ($allPlangs as $plang) {
+                    // if there are any frameworks and they are what we had before ? do the following
+                    if (!empty($k[$progrLang->name])) {
 
-        //         // check if the  programming lang matches with the one in db
+                        // loop through the ssupposedly frameworks that we have
+                        foreach ($k[$progrLang->name] as $frameworkName) {
+                            // get the related framework id
+                            $frameworkId = $progrLang->frameworks->where('name', $frameworkName)->first()->id;
+                            // loop through company frameworks and update the pivot table of ids that match 
+                            foreach ($company->frameworks as $fw) {
+                                $company->frameworks()->updateExistingPivot($frameworkId, ['rating' => $fw->pivot->rating + 1]);
+                            }
+                        }
+                    }
+                } else {
 
-        //         if (array_key_exists($plang->name, $k)) {
-        //             // attach a programming language with the company
+                    $company->plangs()->attach($progrLang->id, ['rating' => 0]);
+                }
+            }
+        } else {
+            foreach ($allPlangs as $plang) {
 
-        //             $company->plangs()->attach($plang->id, ['rating' => 0]);
+                // check if the  programming lang matches with the one in db
 
-        //             // attach the  framework under the programmming language
+                if (array_key_exists($plang->name, $k)) {
+                    // attach a programming language with the company
 
-        //             if (isset($k[$plang->name])) {
+                    $company->plangs()->attach($plang->id, ['rating' => 0]);
 
-        //                 foreach ($k[$plang->name]  as $frameworkName) {
-        //                     // get the id of the framework that matched
-        //                     $framework_id = $plang->frameworks->where('name', $frameworkName)->first()->id;
-        //                     // attach to compnay_framework table 
-        //                     $company->frameworks()->attach($framework_id, ['rating' => 0]);
-        //                 }
-        //             }
-        //             // attach the framework id to the company
+                    // attach the  framework under the programmming language
+                    dd($k);
+                    if (isset($k[$plang->name])) {
 
-        //         }
-        //     }
-        //     //notes of to do
-        //     // revisist "hasManyThrough" "hasManyThrough" should work for getting frameworks through plang and for framework to get company through plang !
-        //     //what if we have frameworks and there is no programming language?  wahala !
+                        foreach ($k[$plang->name]  as $frameworkName) {
+                            // get the id of the framework that matched
+                            $framework_id = $plang->frameworks->where('name', $frameworkName)->first()->id;
+                            // attach to compnay_framework table 
+                            $company->frameworks()->attach($framework_id, ['rating' => 0]);
+                        }
+                    }
+                    // attach the framework id to the company
 
-        //     // find the framework related with that language and match that to the company
+                }
+            }
 
-        //     // if a scraped result have only programming laguages, you should detect the framework
-        //     // after that, changed the model to use "hasManyThrough"
-
-
-        // }
+            // if a scraped result have only programming laguages, you should detect the framework
+            // after that, changed the model to use "hasManyThrough
+        }
 
         dump($company);
 
