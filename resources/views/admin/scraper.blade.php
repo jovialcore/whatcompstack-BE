45a166d1b1e5ce07910db98c699050189054d30b
jovialcore <jollydosky@gmail.com>
2023-08-20 21:13:05 +0100

    removed uncesseary side bar menu

--- resources/views/admin/scraper.blade.php
+++ resources/views/admin/scraper.blade.php
@@ -1,58 +1,39 @@
 @extends('layouts.app')
 @section('content')
-    <div class="col-md-6">
+    <div class="col-md-10 mx-auto">
         <div class="card mb-4">
-            <h5 class="card-header">Scrapper Section</h5>
+            <h5 class="card-header">Scrapper 🪚</h5>
             <div class="card-body">
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlReadOnlyInput1" class="form-label">Read only</label>
-                    <input class="form-control" type="text" id="exampleFormControlReadOnlyInput1"
-                        placeholder="Readonly input here..." readonly />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlReadOnlyInputPlain1" class="form-label">Read plain</label>
-                    <input type="text" readonly class="form-control-plaintext" id="exampleFormControlReadOnlyInputPlain1"
-                        value="email@example.com" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlSelect1" class="form-label">Example select</label>
+                    <label for="exampleFormControlSelect1" class="form-label">Company </label>
                     <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option selected>Choose a source </option>
+                        <option value="1">Source one </option>
+                        <option value="2">Source two</option>
                     </select>
                 </div>
+
                 <div class="mb-3">
-                    <label for="exampleDataList" class="form-label">Datalist example</label>
-                    <input class="form-control" list="datalistOptions" id="exampleDataList"
-                        placeholder="Type to search..." />
-                    <datalist id="datalistOptions">
-                        <option value="San Francisco"></option>
-                        <option value="New York"></option>
-                        <option value="Seattle"></option>
-                        <option value="Los Angeles"></option>
-                        <option value="Chicago"></option>
-                    </datalist>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlSelect2" class="form-label">Example multiple select</label>
-                    <select multiple class="form-select" id="exampleFormControlSelect2"
-                        aria-label="Multiple select example">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    <label for="exampleFormControlSelect1" class="form-label">stack </label>
+                    <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                        <option selected>Choose a stack from here </option>
+                        <option value="1">Backend 🌊 </option>
+                        <option value="2">Frontend 🌪️</option>
+                        <option value="3">Devops 🌤️</option>
+                        <option value="4">Product Design 🌕</option>
+
                     </select>
                 </div>
-                <div>
-                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlSelect1" class="form-label">scrapper source </label>
+                    <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                        <option selected>Choose a source </option>
+                        <option value="1">Source one </option>
+                        <option value="2">Source two</option>
+                    </select>
                 </div>
+                <button type="button" class="btn btn-primary">Shoot !</button>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,7 +121,7 @@
                     <ul class="menu-inner py-1">
                         <!-- Dashboard -->
                         <li class="menu-item active">
-                            <a href="index.html" class="menu-link">
+                            <a href="/" class="menu-link">
                                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                                 <div data-i18n="Analytics">Dashboard</div>
                             </a>
@@ -137,7 +137,7 @@
                         <!-- / Scarping -->
 
                         <!-- Layouts -->
-                        <li class="menu-item">
+                        {{-- <li class="menu-item">
                             <a href="javascript:void(0);" class="menu-link menu-toggle">
                                 <i class="menu-icon tf-icons bx bx-layout"></i>
                                 <div data-i18n="Layouts">Layouts</div>
@@ -170,10 +170,10 @@
                                     </a>
                                 </li>
                             </ul>
-                        </li>
+                        </li> --}}
 
-                        <li class="menu-header small text-uppercase">
-                            <span class="menu-header-text">Pages</span>
+                        {{-- <li class="menu-header small text-uppercase">
+                            <span class="menu-header-text">Page</span>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:void(0);" class="menu-link menu-toggle">
@@ -444,7 +444,7 @@
                                 <i class="menu-icon tf-icons bx bx-file"></i>
                                 <div data-i18n="Documentation">Documentation</div>
                             </a>
-                        </li>
+                        </li> --}}
                     </ul>
                 </aside>
                 <!-- / Menu -->
