d7af66612e066827a9e1f9edef26edf3c6a3bd30
jovialcore <jollydosky@gmail.com>
2023-09-05 08:24:47 +0100

    update on stuf

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    public function create()
+    {
+        return  view('admin.company.addCompany');
+    }
+
+    public function store() : RedirectResponse
+    {
+        return to_route('admin.company.index');
+    }
 }
