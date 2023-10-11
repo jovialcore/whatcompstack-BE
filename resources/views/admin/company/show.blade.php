796d2c14993f3dd8f1cdd808fb4e4b755219836c
jovialcore <jollydosky@gmail.com>
2023-10-11 20:53:09 +0100

    completed viewing for filter

--- resources/views/admin/company/show.blade.php
+++ resources/views/admin/company/show.blade.php
@@ -43,103 +43,33 @@
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane fade show active" id="navs-top-backend" role="tabpanel">
-                        <div class="card">
-                            <h5 class="card-header">Connected Accounts</h5>
-                            <div class="card-body">
-                                <p>Display content from your connected accounts on your site</p>
-                                <!-- Connections -->
-                                <div class="d-flex mb-3">
-                                    <div class="flex-shrink-0">
-                                        <img src="../assets/img/icons/brands/google.png" alt="google" class="me-3"
-                                            height="30" />
+
+                        <p>Display content from your connected accounts on your site</p>
+                        <!-- Connections -->
+                        @foreach ($company->plangs as $plang)
+                            <div class="d-flex mb-3">
+                                <div class="flex-shrink-0">
+                                    <img src="{{ asset('/assets/img/icons/brands/google.png') }}" alt="google"
+                                        class="me-3" height="30" />
+                                </div>
+                                <div class="flex-grow-1 row">
+                                    <div class="col-9 mb-sm-0 mb-2">
+                                        <h6 class="mb-0">{{ $plang->name }}</h6>
+                                        <small class="text-muted">Rated {{ $plang->pivot->rating }}</small>
                                     </div>
-                                    <div class="flex-grow-1 row">
-                                        <div class="col-9 mb-sm-0 mb-2">
-                                            <h6 class="mb-0">Google</h6>
-                                            <small class="text-muted">Calendar and contacts</small>
-                                        </div>
-                                        <div class="col-3 text-end">
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input float-end" type="checkbox" role="switch" />
-                                            </div>
+                                    <div class="col-3 text-end">
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input float-end" type="checkbox" role="switch" />
                                         </div>
                                     </div>
                                 </div>
-                                <div class="d-flex mb-3">
-                                    <div class="flex-shrink-0">
-                                        <img src="../assets/img/icons/brands/slack.png" alt="slack" class="me-3"
-                                            height="30" />
-                                    </div>
-                                    <div class="flex-grow-1 row">
-                                        <div class="col-9 mb-sm-0 mb-2">
-                                            <h6 class="mb-0">Slack</h6>
-                                            <small class="text-muted">Communication</small>
-                                        </div>
-                                        <div class="col-3 text-end">
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input float-end" type="checkbox" role="switch"
-                                                    checked />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex mb-3">
-                                    <div class="flex-shrink-0">
-                                        <img src="../assets/img/icons/brands/github.png" alt="github" class="me-3"
-                                            height="30" />
-                                    </div>
-                                    <div class="flex-grow-1 row">
-                                        <div class="col-9 mb-sm-0 mb-2">
-                                            <h6 class="mb-0">Github</h6>
-                                            <small class="text-muted">Manage your Git repositories</small>
-                                        </div>
-                                        <div class="col-3 text-end">
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input float-end" type="checkbox" role="switch" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex mb-3">
-                                    <div class="flex-shrink-0">
-                                        <img src="../assets/img/icons/brands/mailchimp.png" alt="mailchimp" class="me-3"
-                                            height="30" />
-                                    </div>
-                                    <div class="flex-grow-1 row">
-                                        <div class="col-9 mb-sm-0 mb-2">
-                                            <h6 class="mb-0">Mailchimp</h6>
-                                            <small class="text-muted">Email marketing service</small>
-                                        </div>
-                                        <div class="col-3 text-end">
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input float-end" type="checkbox" role="switch"
-                                                    checked />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0">
-                                        <img src="../assets/img/icons/brands/asana.png" alt="asana" class="me-3"
-                                            height="30" />
-                                    </div>
-                                    <div class="flex-grow-1 row">
-                                        <div class="col-9 mb-sm-0 mb-2">
-                                            <h6 class="mb-0">Asana</h6>
-                                            <small class="text-muted">Communication</small>
-                                        </div>
-                                        <div class="col-3 text-end">
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input float-end" type="checkbox" role="switch"
-                                                    checked />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /Connections -->
                             </div>
-                        </div>
+                        @endforeach
+
+                        <!-- /Connections -->
+
                     </div>
+
                     <div class="tab-pane fade" id="navs-top-profile" role="tabpanel">
                         <p>
                             Donut dragée jelly pie halvah. Danish gingerbread bonbon cookie wafer candy oat cake ice
