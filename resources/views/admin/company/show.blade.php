8911722d9887011795d967727b4f1d9a19027a7b
jovialcore <jollydosky@gmail.com>
2023-10-11 20:54:56 +0100

    unto the next mvp feature, which is adding source

--- resources/views/admin/company/show.blade.php
+++ resources/views/admin/company/show.blade.php
@@ -34,12 +34,7 @@
                             Frontend
                         </button>
                     </li>
-                    <li class="nav-item">
-                        <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
-                            data-bs-target="#navs-top-messages" aria-controls="navs-top-messages" aria-selected="false">
-                            Devops
-                        </button>
-                    </li>
+             
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane fade show active" id="navs-top-backend" role="tabpanel">
@@ -72,31 +67,11 @@
 
                     <div class="tab-pane fade" id="navs-top-profile" role="tabpanel">
                         <p>
-                            Donut dragée jelly pie halvah. Danish gingerbread bonbon cookie wafer candy oat cake ice
-                            cream. Gummies halvah tootsie roll muffin biscuit icing dessert gingerbread. Pastry ice
-                            cream
-                            cheesecake fruitcake.
+                           No stack for frontend for now 
                         </p>
-                        <p class="mb-0">
-                            Jelly-o jelly beans icing pastry cake cake lemon drops. Muffin muffin pie tiramisu
-                            halvah
-                            cotton candy liquorice caramels.
-                        </p>
+                      
                     </div>
-                    <div class="tab-pane fade" id="navs-top-messages" role="tabpanel">
-                        <p>
-                            Oat cake chupa chups dragée donut toffee. Sweet cotton candy jelly beans macaroon
-                            gummies
-                            cupcake gummi bears cake chocolate.
-                        </p>
-                        <p class="mb-0">
-                            Cake chocolate bar cotton candy apple pie tootsie roll ice cream apple pie brownie cake.
-                            Sweet
-                            roll icing sesame snaps caramels danish toffee. Brownie biscuit dessert dessert. Pudding
-                            jelly
-                            jelly-o tart brownie jelly.
-                        </p>
-                    </div>
+              
                 </div>
             </div>
         </div>
