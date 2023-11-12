fca94456e27bfc24bc2e78970163a6a7ad09ec04
jovialcore <jollydosky@gmail.com>
2023-11-12 20:58:58 +0100

    my open telemetry console is working

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Services\DashboardService;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+use OpenTelemetry\API\Globals;
+use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporterFactory;
 use OpenTelemetry\SDK\Trace\TracerProviderFactory;
 
 
@@ -13,34 +16,29 @@ class DashboardController extends Controller
 {
 
     protected  $dashboardService;
+    public $tracer;
     public function __construct()
     {
         $this->dashboardService =   new DashboardService;
+
+        $this->tracer =  Globals::tracerProvider()->getTracer('whatcompany_Stack');
     }
 
 
     public function index(): View
     {
 
-        Http::withTrace()->get('https://google.com');
-
-        $traceFactory = new TracerProviderFactory();
-        dd($traceFactory);
-        $tracerProvider = $traceFactory->create();
-        $tracer = $tracerProvider->getTracer('whatcompanystack_trace');
-
-        $span = $tracer->spanBuilder('get_all_details')->startSpan();
+        $span =  $this->tracer->spanBuilder('first_span')->startSpan();
 
         $result = random_int(1, 6);
 
         $data = $this->dashboardService->getAppStats();
 
-        $span->setAttribute('get_all_details_id', $result);
+        $exporter = new  ConsoleSpanExporterFactory;
 
-        $span->end();
-
+        $span->addEvent('called the home page', ['result' => $result])
+            ->end();
+       
         return view('admin.dashboard', $data);
-
-        $tracerProvider->shutDown();
     }
 }
