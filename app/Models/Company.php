d08795d7fd7e147ff7f945d85a89857149e5f95f
jovialcore <jollydosky@gmail.com>
2023-12-06 20:25:10 +0100

    reduce the scope so that only companies with atleast one backend programming language shows up here

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -45,17 +45,14 @@ class Company extends Model
 
     public function plangs() // plangs via company
     {
-
         $this->wasRecentlyCreated;
         return $this->belongsToMany(Plang::class, 'plang_company')->withPivot(['draft_rating', 'is_draft', 'is_published',  'rating']);
     }
 
-
-    public function frameworks() // frameworks via company
+    public function frameworks() // frameworks via company // this is for be right
     {
         return $this->belongsToMany(Framework::class, 'framework_company')->withPivot(['draft_rating', 'is_draft', 'is_published', 'rating']);
     }
-
 
     public function feFrameworks(): BelongsToMany
     {
@@ -64,7 +61,7 @@ class Company extends Model
 
     public function scopeFetchAllClientDetails($query)
     {
-        return $query->with('plangs', function ($query) {
+        return $query->withWhereHas('plangs', function ($query) {
             $query->where('is_published', 1);
         })->with('frameworks', function ($query) {
             $query->where('is_published', 1);
