1308b15574feb5cda00f137dd278878fb235f054
jovialcore <jollydosky@gmail.com>
2023-12-13 20:05:43 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+Auth::routes('register' => false);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
