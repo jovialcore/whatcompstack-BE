91698a15dce432e251c5da3a48cf710d99c70558
jovialcore <jollydosky@gmail.com>
2023-12-19 20:26:19 +0100

    added mobile stack seederc

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
         $this->call(CompanyTableSeeder::class);
         $this->call(PlangSeeder::class);
         $this->call(FrameworkAndProgrammingLangSeeder::class);
