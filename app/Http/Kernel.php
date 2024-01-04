905e8f62fb25a81a3df8c466fcd74aa65da15dc6
Chidiebere Chukwudi <chidideveloper@gmail.com>
2024-01-04 12:25:09 +0100

    create a middleware for authenticated users to be redirected to the dashboard once they access the /not-an-admin route (#27)
    Co-authored-by: codejutsu1 <codejutsu@protonmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Roles::class,
+        'not.an.admin' => \App\Http\Middleware\NotAnAdminMiddleware::class
     ];
 }
---
+++ app/Http/Middleware/NotAnAdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use Symfony\Component\HttpFoundation\Response;
+
+class NotAnAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role == 'admin') {
+            return redirect(RouteServiceProvider::HOME);
+        }
+
+        return $next($request);
+    }
+}
