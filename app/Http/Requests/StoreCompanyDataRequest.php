779b8a5b0a6cd78c8b0d0f3143ba8c238ee13b79
Dunu Daniel <88636723+codejutsu1@users.noreply.github.com>
2024-01-10 08:30:33 +0100

    Refactor/store company data validation (#32)
    * create a middleware for authenticated users to be redirected to the dashboard once they access the /not-an-admin route (#27)
    
    Co-authored-by: codejutsu1 <codejutsu@protonmail.com>
    
    * moved the store company data validation rules to a request file
    
    ---------
    
    Co-authored-by: Chidiebere Chukwudi <chidideveloper@gmail.com>

---
+++ app/Http/Requests/StoreCompanyDataRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompanyDataRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'about' => 'required',
+            'url' => 'url|unique:companies,url',
+            'ceo_name' => 'string|nullable',
+            'cto_contact' => 'url|nullable',
+            'cto_name' => 'string|nullable',
+            'hr_name' => 'string|nullable',
+            'hr_contact' => 'url |nullable',
+            'logo' => 'required|mimes:png,jpeg,svg',
+            'source_slug' => 'nullable|string|unique:companies,source_slug'
+        ];
+    }
+}
