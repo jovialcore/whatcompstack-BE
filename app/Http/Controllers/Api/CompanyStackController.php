a1ccd14cbe043b282b0e6ee3ecfce78a2c96c504
Omotayo Obafemi <omotayo@testlio.com>
2024-12-08 20:45:57 +0000

    Added Search Functionality to Get All Companies Endpoint

--- app/Http/Controllers/Api/CompanyStackController.php
+++ app/Http/Controllers/Api/CompanyStackController.php
@@ -11,22 +11,42 @@ use Illuminate\Http\Request;
 
 class CompanyStackController extends Controller
 {
+    private function get_companies_by_search_term($term, Company $company)
+    {
+        $term = strtolower($term);
+        $companies = $company->FetchAllClientDetails()
+            ->where(function ($query) use ($term) {
+                $query->where('name', 'LIKE', "%{$term}%")
+                    ->orWhereHas('plangs', function ($query) use ($term) {
+                        $query->where('name', 'LIKE', "%{$term}%");
+                    })
+                    ->orWhereHas('frameworks', function ($query) use ($term) {
+                        $query->where('name', 'LIKE', "%{$term}%");
+                    })
+                    ->orWhereHas('feFrameworks', function ($query) use ($term) {
+                        $query->where('name', 'LIKE', "%{$term}%");
+                    })
+                    ->orWhereHas('mobilePlangs', function ($query) use ($term) {
+                        $query->where('name', 'LIKE', "%{$term}%");
+                    });
+        });
+        return $companies;
+    }
 
     public function index(Request $req, Company $company)
     {
-
-        $companies = $company::FetchAllClientDetails();
-
-        //if laravel had an "orWithWhereHAs" ðŸ™‚
-
-        if ($companies->exists() > 0) {
-
-            $companies = $companies->paginate(21);
-
-            return CompanyResource::collection($companies);
-        } else {
-            return response()->json(['message' => 'No  Results found'], 200);
+        $search_term = $req->query('term');
+        if ($search_term) {
+            $paginated_companies = $this->get_companies_by_search_term($search_term, $company)->paginate(21);
+            return CompanyResource::collection($paginated_companies);
         }
+
+        $companies = $company->FetchAllClientDetails();
+        if ($companies->exists() === 0) {
+            return response()->json(['message' => 'No  Results found'], 404);
+        }
+        $companies = $companies->paginate(21);
+        return CompanyResource::collection($companies);
     }
 
     public function show($source_slug)
--- routes/api.php
+++ routes/api.php
@@ -67,3 +67,6 @@ Route::middleware('auth:sanctum')->prefix('/community/member')->group(function (
 
 
 Route::get('/company/stack/details/{source_slug}', [App\Http\Controllers\Api\CompanyStackController::class, 'show']);
+Route::get('/companies', [App\Http\Controllers\Api\CompanyStackController::class, 'index']);
+Route::get('/companies/search', [App\Http\Controllers\Api\CompanyStackController::class, 'findByName']);
+
